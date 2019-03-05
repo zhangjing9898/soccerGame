a16842a99d71e45566bbf36f253414abcc8c5844
zhangjing <1105249307@qq.com>
2019-03-05 11:03:23 +0800

    feat: add ball.ts construct

--- bin-debug/ball.js
+++ bin-debug/ball.js
@@ -11,13 +11,56 @@ r.prototype = e.prototype, t.prototype = new r();
 var ball = (function (_super) {
     __extends(ball, _super);
     function ball() {
-        return _super.call(this) || this;
+        var _this = _super.call(this) || this;
+        _this.i = 1;
+        return _this;
     }
     ball.prototype.partAdded = function (partName, instance) {
         _super.prototype.partAdded.call(this, partName, instance);
     };
     ball.prototype.childrenCreated = function () {
         _super.prototype.childrenCreated.call(this);
+        this.btn_start.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+    };
+    ball.prototype.onButtonClick = function () {
+    };
+    ball.prototype.tweenFn = function (img, btn) {
+        var _this = this;
+        this.tw = egret.Tween.get(img);
+        this.tw.to({
+            y: 1000
+        }, 500)
+            .to({
+            y: 120
+        }, 500)
+            .to({
+            y: 1000
+        }, 500)
+            .to({
+            y: 120
+        }, 500)
+            .to({
+            y: 1000
+        }, 500)
+            .to({
+            y: 120
+        }, 500)
+            .call(function () {
+            btn.visible = true;
+            _this.btn_start.currentState = "reset";
+            _this.i = 4;
+        });
+    };
+    // 足球运动fn
+    ball.prototype.ballTween = function () {
+        this.random = Math.random();
+        this.tw = egret.Tween.get(this.img_ball);
+        if (this.random < 0.5) {
+            this.tweenFn(this.img_ball, this.win_Buttom);
+        }
+        else {
+            this.tweenFn(this.img_ball, this.win_Top);
+        }
     };
     return ball;
 }(eui.Component));
--- src/ball.ts
+++ src/ball.ts
@@ -1,4 +1,15 @@
 class ball extends eui.Component implements  eui.UIComponent {
+	
+	public btn_start: eui.Button;
+	public img_ball: eui.Image;
+	public win_Top: eui.Image;
+	public win_Buttom: eui.Image;
+	public i: number = 1;
+	public tw: egret.Tween;
+
+	// 用于判断输赢的 0 到 1 之间的随机数
+	random: number;
+
 	public constructor() {
 		super();
 	}
@@ -12,6 +23,48 @@ class ball extends eui.Component implements  eui.UIComponent {
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
+		this.btn_start.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+	}
+
+	private onButtonClick() {
+
 	}
 	
+	private tweenFn(img: eui.Image, btn: eui.Image) {
+		this.tw = egret.Tween.get(img);
+		this.tw.to({
+			y: 1000
+		},500)
+		.to({
+			y: 120
+		}, 500)
+		.to({
+			y: 1000
+		}, 500)
+		.to({
+			y: 120
+		}, 500)
+		.to({
+			y: 1000
+		}, 500)
+		.to({
+			y: 120
+		}, 500)
+		.call(()=>{
+			btn.visible = true;
+			this.btn_start.currentState = "reset";
+			this.i = 4;
+		})
+	}
+
+	// 足球运动fn
+	private ballTween() {
+		this.random = Math.random();
+		this.tw = egret.Tween.get(this.img_ball);
+		if(this.random < 0.5) {
+			this.tweenFn(this.img_ball, this.win_Buttom);
+		} else {
+			this.tweenFn(this.img_ball, this.win_Top);
+		}
+	}
 }
